614780281cfd6d6a1bfcbcf6bdec2920bc1f3d5f
matthewheubach <mattheubach@live.com>
2021-12-01 13:26:06 -0800

    Update pcloud2dist to use median range/angle data

--- matlab/pcloud2dist.m
+++ matlab/pcloud2dist.m
@@ -11,8 +11,8 @@ dist=sqrt(x.^2 + y.^2 + z.^2);
 psi=rad2deg(atan2(y,x));
 point_dist_psi=[xyz dist psi];
 S1 = point_dist_psi(point_dist_psi(:,4) >min_dist,4:5);
-dist=mean(S1(:,1));
-psi=mean(S1(:,2));
+dist=median(S1(:,1));
+psi=median(S1(:,2));
 
 end
 
